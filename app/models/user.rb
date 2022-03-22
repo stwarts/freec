d4728a18758355d5db048161a17c56bed7399eec
Cuong Nguyen <cuong.nguyen@oivan.com>
2022-03-22 21:54:45 +0700

    Add user table and model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,18 @@
 class User < ApplicationRecord
+  enum :user_type, { admin: 'admin', user: 'user' }
+
+  DEFAULT_TYPE = user_types.fetch(:user)
+
+  has_secure_password
+
+  validates :email, presence: true, uniqueness: true
+  validates :user_type, presence: true
+
+  before_validation :set_user_type
+
+  private
+
+  def set_user_type
+    self.user_type = DEFAULT_TYPE
+  end
 end
--- db/migrate/20220322144417_create_users.rb
+++ db/migrate/20220322144417_create_users.rb
@@ -1,11 +1,13 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-      t.string :email
-      t.string :password
+      t.string :email, null: false
+      t.string :password, null: false
       t.string :user_type
 
       t.timestamps
     end
+
+    add_index :users, :email, unique: true
   end
 end
