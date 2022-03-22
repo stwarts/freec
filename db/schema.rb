1d2eb23ed6d72b969a480ed148f01345f1eafcfd
Cuong Nguyen <cuong.nguyen@oivan.com>
2022-03-22 22:15:31 +0700

    Add name to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_22_144417) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_22_151339) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_22_144417) do
     t.string "user_type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
