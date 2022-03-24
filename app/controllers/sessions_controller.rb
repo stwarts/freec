e9157f1b87b6cd04bf80ac97df5483c249b8f9c5
Cuong Nguyen <cuong.nguyen@oivan.com>
2022-03-24 20:53:05 +0700

    Return unauthorized instead of not_found in SessionsController#create when email not exist

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,8 @@
 class SessionsController < ApplicationController
   def create
-    user = User.find_by!(email: parmas[:email])
-    user.authenticate(params[:password])
+    user = User.find_by(email: parmas[:email])
 
-    if user.authenticate(params[:password])
+    if user&.authenticate(params[:password])
       render json: user.authenticate_token
     else
       head :unauthorized
